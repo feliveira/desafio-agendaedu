35e0d7253fcf5b15610cf4dcfe224ce21cc32c03
Felipe Oliveira <feliveira8@gmail.com>
2025-07-04 02:10:43 -0300

    :sparkles: feat: integrating Sentry

--- App.tsx
+++ App.tsx
@@ -10,10 +10,25 @@ import { Provider } from "react-redux";
 
 import { Poppins_400Regular, Poppins_600SemiBold, Poppins_700Bold, useFonts } from "@expo-google-fonts/poppins";
 import * as SplashScreen from "expo-splash-screen";
+import * as Sentry from '@sentry/react-native';
+
+const SENTRY_DSN = process.env.EXPO_PUBLIC_SENTRY_DSN;
+
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    sendDefaultPii: true,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  });
+} else {
+  console.warn("SENTRY_DSN not set, Sentry will not be initialized.");
+}
 
 SplashScreen.preventAutoHideAsync();
 
-export default function App() {
+export default Sentry.wrap(function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_600SemiBold,
@@ -35,4 +50,4 @@ export default function App() {
       </NavigationContainer>
     </Provider>
   );
-}
+});
